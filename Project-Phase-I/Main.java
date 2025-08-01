0b8fe72b32664be73736f97d61e90d14035791f4
Giridharan.R <gridharan.r@sece.ac.in>
2025-08-01 13:03:48 +0530

    Mian.java

---
+++ Project-Phase-I/Main.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String args[]){
+        File userFile = new File("userfile.txt");
+        File userTypeFile = new File("usertype.txt");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+    
+}
